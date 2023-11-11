119f129573a7eb497cb90e01fefe7905c8e19b33
iuboyraz <iuboyraz@gmail.com>
2023-11-11 16:22:57 +0300

    _02_PositionManagement and _03_Attestations were completed

--- src/test/java/Campus/_02_PositionManagement.java
+++ src/test/java/Campus/_02_PositionManagement.java
@@ -1,4 +1,149 @@
 package Campus;
 
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.*;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.*;
+
+import java.util.*;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class _02_PositionManagement {
+    Faker randomGenerator = new Faker();
+    String positionCategoryId;
+    RequestSpecification requestSpec;
+    String rndPositionCategoryName;
+    Map<String, String> newPositionCategory;
+    @BeforeClass
+    public void setUp() {
+        baseURI = "https://test.mersys.io/";
+
+        Map<String, String> userCredential = new HashMap<>();
+        userCredential.put("username", "turkeyts");
+        userCredential.put("password", "TechnoStudy123");
+        userCredential.put("rememberMe", "true");
+
+        Cookies cookies =
+                given()
+                        .body(userCredential)
+                        .contentType(ContentType.JSON)
+
+                        .when()
+                        .post("/auth/login")
+
+                        .then()
+                        .statusCode(200)
+                        .extract().response().detailedCookies()
+                ;
+
+        requestSpec = new RequestSpecBuilder()
+                .addCookies(cookies)
+                .setContentType(ContentType.JSON)
+                .build()
+        ;
+    }
+    @Test
+    public void createPositionCategory(){
+
+        rndPositionCategoryName = randomGenerator.job().field() + randomGenerator.number().digits(5);
+
+        newPositionCategory = new HashMap<>();
+        newPositionCategory.put("name", rndPositionCategoryName);
+
+        positionCategoryId =
+                given()
+
+                        .spec(requestSpec)
+                        .body(newPositionCategory)
+
+                        .when()
+                        .post("school-service/api/position-category/")
+
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .extract().path("id")
+        ;
+        System.out.println("positionCategoryId = " + positionCategoryId);
+    }
+    @Test (dependsOnMethods = "createPositionCategory")
+    public void createPositionCategoryNegative(){
+
+        given()
+
+                .spec(requestSpec)
+                .body(newPositionCategory)
+
+                .when()
+                .put("school-service/api/position-category/")
+
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("message", containsString("Please, provide valid data to update 'Position Category', it's not created to update"))
+
+        ;
+        System.out.println("positionCategoryId = " + positionCategoryId);
+    }
+    @Test (dependsOnMethods = "createPositionCategoryNegative")
+    public void updatePositionCategory(){
+        String newPositionCategoryName = randomGenerator.job().field() + randomGenerator.number().digits(5);
+
+        Map<String, String> updatePositionCategory = new HashMap<>();
+        updatePositionCategory.put("id", positionCategoryId);
+        updatePositionCategory.put("name", newPositionCategoryName);
+
+        given()
+
+                .spec(requestSpec)
+                .body(updatePositionCategory)
+
+                .when()
+                .put("school-service/api/position-category/")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("name", equalTo(newPositionCategoryName))
+
+        ;
+        System.out.println("positionCategoryId = " + positionCategoryId);
+        System.out.println("newPositionCategoryName = " + newPositionCategoryName);
+    }
+    @Test (dependsOnMethods = "updatePositionCategory")
+    public void deletePositionCategory(){
+
+        given()
+                .spec(requestSpec)
+
+                .when()
+                .delete("school-service/api/position-category/" + positionCategoryId)
+
+                .then()
+                .log().body()
+                .statusCode(204)
+
+        ;
+        System.out.println("positionCategoryId = " + positionCategoryId);
+    }
+    @Test (dependsOnMethods = "deletePositionCategory")
+    public void deletePositionCategoryNegative(){
+
+        given()
+                .spec(requestSpec)
+
+                .when()
+                .delete("school-service/api/position-category/" + positionCategoryId)
+
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("message", containsString("PositionCategory not  found"))
+
+        ;
+        System.out.println("positionCategoryId = " + positionCategoryId);
+    }
 }
--- src/test/java/Campus/_03_Attestations.java
+++ src/test/java/Campus/_03_Attestations.java
@@ -1,4 +1,149 @@
 package Campus;
 
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.*;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.*;
+
+import java.util.*;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class _03_Attestations {
+    Faker randomGenerator = new Faker();
+    String attestationId;
+    RequestSpecification requestSpec;
+    String rndAttestationName;
+    Map<String, String> newAttestation;
+    @BeforeClass
+    public void setUp() {
+        baseURI = "https://test.mersys.io/";
+
+        Map<String, String> userCredential = new HashMap<>();
+        userCredential.put("username", "turkeyts");
+        userCredential.put("password", "TechnoStudy123");
+        userCredential.put("rememberMe", "true");
+
+        Cookies cookies =
+                given()
+                        .body(userCredential)
+                        .contentType(ContentType.JSON)
+
+                        .when()
+                        .post("/auth/login")
+
+                        .then()
+                        .statusCode(200)
+                        .extract().response().detailedCookies()
+                ;
+
+        requestSpec = new RequestSpecBuilder()
+                .addCookies(cookies)
+                .setContentType(ContentType.JSON)
+                .build()
+        ;
+    }
+    @Test
+    public void createAttestation(){
+
+        rndAttestationName = randomGenerator.name().fullName() + randomGenerator.number().digits(5);
+
+        newAttestation = new HashMap<>();
+        newAttestation.put("name", rndAttestationName);
+
+        attestationId =
+                given()
+
+                        .spec(requestSpec)
+                        .body(newAttestation)
+
+                        .when()
+                        .post("school-service/api/attestation/")
+
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .extract().path("id")
+        ;
+        System.out.println("attestationId = " + attestationId);
+    }
+    @Test (dependsOnMethods = "createAttestation")
+    public void createAttestationNegative(){
+
+        given()
+
+                .spec(requestSpec)
+                .body(newAttestation)
+
+                .when()
+                .put("school-service/api/attestation/")
+
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("message", containsString("Please, provide valid data to update 'Attestation', your 'Attestation' is not created to update"))
+
+        ;
+        System.out.println("attestationId = " + attestationId);
+    }
+    @Test (dependsOnMethods = "createAttestationNegative")
+    public void updateAttestation(){
+        String newAttestationName = randomGenerator.job().field() + randomGenerator.number().digits(5);
+
+        Map<String, String> updateAttestation = new HashMap<>();
+        updateAttestation.put("id", attestationId);
+        updateAttestation.put("name", newAttestationName);
+
+        given()
+
+                .spec(requestSpec)
+                .body(updateAttestation)
+
+                .when()
+                .put("school-service/api/attestation/")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("name", equalTo(newAttestationName))
+
+        ;
+        System.out.println("positionCategoryId = " + attestationId);
+        System.out.println("newAttestationName = " + newAttestationName);
+    }
+    @Test (dependsOnMethods = "updateAttestation")
+    public void deleteAttestation(){
+
+        given()
+                .spec(requestSpec)
+
+                .when()
+                .delete("school-service/api/attestation/" + attestationId)
+
+                .then()
+                .log().body()
+                .statusCode(204)
+
+        ;
+        System.out.println("attestationId = " + attestationId);
+    }
+    @Test (dependsOnMethods = "deleteAttestation")
+    public void deleteAttestationNegative(){
+
+        given()
+                .spec(requestSpec)
+
+                .when()
+                .delete("school-service/api/attestation/" + attestationId)
+
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("message", containsString("attestation not found"))
+
+        ;
+        System.out.println("attestationId = " + attestationId);
+    }
 }
